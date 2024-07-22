92b6be3535130a3125144f55a67a9ec8483888d3
zant <gonzalochristobal@gmail.com>
2024-07-22 15:52:06 -0400

    feat: add more icons from design system

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,15 +1,25 @@
 /* 
   Helper component to render SVG icons
   when importing a new svg icon, add it to the IconMap object
-  and that <svg /> and <path /> elements have the "fill" attribute set to "current"
+  and that <svg /> and <path /> elements have the "fill" attribute set to "current" 
 */
 
 import Success from '@/assets/icons/success.svg?react';
 import Verified from '@/assets/icons/verified.svg?react';
+import Hello from '@/assets/icons/hello.svg?react';
+import City from '@/assets/icons/city.svg?react';
+import Community from '@/assets/icons/community.svg?react';
+import Data from '@/assets/icons/data.svg?react';
+import Register from '@/assets/icons/register.svg?react';
 
 const IconMap = {
   Verified,
   Success,
+  Hello,
+  City,
+  Community,
+  Data,
+  Register,
 };
 
 const Icon = ({
