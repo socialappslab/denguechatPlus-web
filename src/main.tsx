c525415910f7fbb67db9a00402cec1292c8739f5
zant <gonzalochristobal@gmail.com>
2024-11-06 18:14:12 +0100

    feat: setup rollbar

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Provider } from '@rollbar/react';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -17,10 +18,27 @@ dayjs.locale(import.meta.env.VITE_DEFAULT_LANG ?? 'es');
 const rootElement = document.getElementById('root-app') as HTMLElement;
 const root = ReactDOM.createRoot(rootElement);
 
+const rollbarConfig = {
+  accessToken: import.meta.env.ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    client: {
+      javascript: {
+        code_version: '1.0.0',
+        source_map_enabled: true,
+      },
+    },
+  },
+};
+
 root.render(
   <StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={import.meta.env.VITE_DEFAULT_LANG ?? 'es'}>
-      <AppRouter />
-    </LocalizationProvider>
+    <Provider config={rollbarConfig}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={import.meta.env.VITE_DEFAULT_LANG ?? 'es'}>
+        <AppRouter />
+      </LocalizationProvider>
+    </Provider>
+    ,
   </StrictMode>,
 );
