03c6afac03cf07b6e1ea7293408b430562d85646
zant <gonzalochristobal@gmail.com>
2024-11-12 14:28:29 +0100

    feat: add tooltip to progress bar

--- src/themed/progress-bar/ProgressBar.tsx
+++ src/themed/progress-bar/ProgressBar.tsx
@@ -1,18 +1,27 @@
-import { Box } from '@mui/material';
+import { Box, Tooltip } from '@mui/material';
 import React from 'react';
 import Text from '../text/Text';
+import Help from '@/assets/icons/help.svg';
 
 export interface ProgressBarProps {
   label: string;
   progress: number;
   color: string;
+  tooltip?: string;
 }
 
-export const ProgressBar: React.FC<ProgressBarProps> = ({ label, progress, color }) => {
+export const ProgressBar: React.FC<ProgressBarProps> = ({ label, progress, color, tooltip }) => {
   return (
     <Box className="flex-col items-start mb-4">
       <Box className="flex flex-row justify-between">
-        <Text className="text-gray-800 font-medium mr-4 mb-2">{label}</Text>
+        <Box className="flex">
+          <Text className="text-gray-800 font-medium mr-2 mb-0">{label}</Text>
+          {tooltip && (
+            <Tooltip title={tooltip} placement="top">
+              <img className="self-start mt-1" src={Help} alt="help" width="18" />
+            </Tooltip>
+          )}
+        </Box>
         <Text className="text-gray-800 font-medium ml-4">{progress}</Text>
       </Box>
       <Box className="flex-row items-center">
