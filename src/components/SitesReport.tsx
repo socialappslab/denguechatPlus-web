ed7975f1ff3a533c8fdb68b432836a7cd4c45d22
Raul Ferreira G <ferreirarauldev@gmail.com>
2025-02-13 16:28:53 -0300

    fix: fix the labels in statics graphics

--- src/components/SitesReport.tsx
+++ src/components/SitesReport.tsx
@@ -23,7 +23,7 @@ const SitesReport = () => {
 
   return (
     <Box className="border-solid border-neutral-100 rounded-md p-6 mb-10">
-      <Title label={t('sitesReport.title')} type="subsection" className="mb-0" />
+      <Title label={t('sitesReport.shortTitle')} type="subsection" className="mb-0" />
       <Box className="flex flex-col mt-6">
         <>
           <ProgressBar
@@ -32,7 +32,7 @@ const SitesReport = () => {
             progress={data?.tarikiPercentage ?? 0}
             color="bg-green-600"
             tooltip={t('sitesReport.tarikiSiteInfo')}
-            description={`${data?.tarikiHousesQty} ${t('sitesReport.description')} ${data?.totalHousesQty}`}
+            description={`${data?.tarikiHousesQty} ${t('sitesReport.description')} ${data?.totalHousesQty} ${t('sitesReport.sites')}`}
           />
           <ProgressBar
             label={t('sitesReport.quantity')}
@@ -40,7 +40,7 @@ const SitesReport = () => {
             progress={data?.greenContainerPercentage ?? 0}
             color="bg-green-800"
             tooltip={t('sitesReport.greenContainersInfo')}
-            description={`${data?.greenContainerQty} ${t('sitesReport.description')} ${data?.totalContainerQty}`}
+            description={`${data?.greenContainerQty} ${t('sitesReport.description')} ${data?.totalContainerQty} ${t('sitesReport.containers')}`}
           />
         </>
       </Box>
