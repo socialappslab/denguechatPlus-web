6ab2201b7443967b116e8bda30363ed20541d8ad
Raul Ferreira G <ferreirarauldev@gmail.com>
2025-03-05 15:58:58 -0300

    feat: send x-client-device in the header

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -15,6 +15,7 @@ export const globalConfig: RetryConfig = {
   baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
+    'X-Client-Device': 'web',
   },
 };
 
