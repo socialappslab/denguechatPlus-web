064c5d3e8c3fc5d93a5ee4aa9a29991227df43bb
Raul Ferreira G <ferreirarauldev@gmail.com>
2025-03-19 13:28:04 -0300

    feat: integration with sentry

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // @ts-ignore
 /**
